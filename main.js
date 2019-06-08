31e644417a0e9847ff8e8b0dbbbedce12c609c6f
j21chan <j94chan@gmail.com>
2019-06-08 11:51:38 +0900

    불필요한 코드 정리 및 리팩토링

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ var fs = require('fs'),
     originCollection,
     updatedCollection;
 
-// updatedCollection : 스웨거에서 변환 된 컬렉션 (추가/삭제/수정 된 API가 있을 수 있음)
 // originCollection : 기존 컬렉션 (테스트 코드, 요청 등이 셋팅 되어져 있음)
+// updatedCollection : 스웨거에서 변환 된 컬렉션 (추가/삭제/수정 된 API가 있을 수 있음)
 originCollection = new Collection(JSON.parse(fs.readFileSync('./collection_folder/origin collection.postman_collection.json').toString()));
 updatedCollection = new Collection(JSON.parse(fs.readFileSync('./collection_folder/swagger collection.postman_collection.json').toString()));
 
@@ -26,26 +26,15 @@ addedList.forEach(add => console.log(add.request.url.getRaw()));
 console.log('');
 
 console.log('삭제된 리퀘스트: ');
-deletedParentList = [];
 deletedList = originList.filter(d => !updatedList.find(m => m.request.url.getRaw() == d.request.url.getRaw()));
-deletedList.forEach(d => deletedParentList.push(d.parent()));
-// console.log(deletedList[0])
-// console.log(deletedParentList);
-deletedList.forEach(item => {
-    item.forEachParent(parent => {
-        parent.items.remove(item.id);
-        console.log(parent);
-    })
-})
 deletedList.forEach(item => console.log(item.request.url.getRaw()));
 console.log('');
 
+// 추가된 request 추가
 addedList.forEach(added => originCollection.items.add(added));
+// 삭제된 request 삭제
 deletedList.forEach(deleted => originCollection.items.remove(deleted.id));
-// deletedParentList.forEach(deletedItemGroup => originCollection.items.remove(deletedItemGroup.id));
-// originCollection.items.assimilate(updatedCollection.items, true);
-// updatedCollection.items.assimilate(originCollection.items, true);
-// console.log(updatedCollection.items.members);
+deletedList.forEach(item => item.forEachParent(parent => parent.items.remove(item.id)));
 
 console.log('최신화된 컬렉션: ');
 originCollection.forEachItem(item => console.log(item.request.url.getRaw()));
